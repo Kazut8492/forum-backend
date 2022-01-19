2341e3244a9d35f7dfdd7745d4519cf5fd7107b4
Kazuki Taira <kazuki@lift99.co>
2022-01-19 15:11:41 +0200

    user cannot like / dislike at the same time. Also, user can cancel reaction by pressing the button again

--- src/handle_dislikeComment.go
+++ src/handle_dislikeComment.go
@@ -35,22 +35,40 @@ func DisLikeCommentHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Insert dislike
-	statement, err := db.Prepare(`
-		INSERT INTO dislike (
-			post_id,
-			comment_id,
-			creator_username
-		) VALUES (?, ?, ?)
-	`)
-	if err != nil {
-		log.Fatal(err.Error())
+	// If the user already liked it then erase it
+	var matchedUsernameLiked string
+	db.QueryRow("SELECT creator_username FROM like WHERE creator_username = ? AND post_id = ? AND comment_id = ?", matchedUsername, postID, commentID).Scan(&matchedUsernameLiked)
+	if matchedUsernameLiked == matchedUsername {
+		_, err = db.Exec("DELETE FROM like WHERE creator_username = ? AND post_id = ? AND comment_id = ?", matchedUsername, postID, commentID)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
 	}
-	defer statement.Close()
-	// number of variables have to be matched with INSERTed variables
-	statement.Exec(postID, commentID, matchedUsername)
 
-	db.Exec("UPDATE comment SET dislike = dislike + 1 WHERE post_id = ? AND comment_id = ?", postID, commentID)
+	// If the user already disliked it, then erase it.
+	var matchedUsernameDisliked string
+	db.QueryRow("SELECT creator_username FROM dislike WHERE creator_username = ? AND post_id = ? AND comment_id = ?", matchedUsername, postID, commentID).Scan(&matchedUsernameDisliked)
+	if matchedUsernameDisliked == matchedUsername {
+		_, err = db.Exec("DELETE FROM dislike WHERE creator_username = ? AND post_id = ? AND comment_id = ?", matchedUsername, postID, commentID)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	} else {
+		// Insert dislike
+		statement, err := db.Prepare(`
+			INSERT INTO dislike (
+				post_id,
+				comment_id,
+				creator_username
+			) VALUES (?, ?, ?)
+		`)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		defer statement.Close()
+		// number of variables have to be matched with INSERTed variables
+		statement.Exec(postID, commentID, matchedUsername)
+	}
 
 	http.Redirect(w, r, "/post?id="+postID, http.StatusFound)
 }
--- src/handle_likePost.go
+++ src/handle_likePost.go
@@ -40,19 +40,39 @@ func LikePostHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Insert like
-	statement, err := db.Prepare(`
-		INSERT INTO like (
-			post_id,
-			creator_username
-		) VALUES (?, ?)
-	`)
-	if err != nil {
-		log.Fatal(err.Error())
+	// If the user already disliked it then erase it
+	var matchedUsernameDisliked string
+	db.QueryRow("SELECT creator_username FROM dislike WHERE creator_username = ? AND post_id = ? AND comment_id = 0", matchedUsername, postID).Scan(&matchedUsernameDisliked)
+	if matchedUsernameDisliked == matchedUsername {
+		_, err = db.Exec("DELETE FROM dislike WHERE creator_username = ? AND post_id = ? AND comment_id = 0", matchedUsername, postID)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
 	}
-	defer statement.Close()
-	// number of variables have to be matched with INSERTed variables
-	statement.Exec(postID, matchedUsername)
+
+	// If the user already liked it, then erase it.
+	var matchedUsernameLiked string
+	db.QueryRow("SELECT creator_username FROM like WHERE creator_username = ? AND post_id = ? AND comment_id = 0", matchedUsername, postID).Scan(&matchedUsernameLiked)
+	if matchedUsernameLiked == matchedUsername {
+		_, err = db.Exec("DELETE FROM like WHERE creator_username = ? AND post_id = ? AND comment_id = 0", matchedUsername, postID)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	} else {
+		// Insert like
+		statement, err := db.Prepare(`
+			INSERT INTO like (
+				post_id,
+				creator_username
+			) VALUES (?, ?)
+		`)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		defer statement.Close()
+		// number of variables have to be matched with INSERTed variables
+		statement.Exec(postID, matchedUsername)
+	}
 
 	switch {
 	case fromPage == "index":
